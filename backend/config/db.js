8c10cbccd8374bdc3e1db8841538535f49f4b39b
devkaikyy <kaikyhernaide151@gmail.com>
2026-05-30 17:52:56 -0300

    Corrige validação de login

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connectDB(){
     try {
-        mongoose.connect('mongodb://127.0.0.1:27017/banco02')
+        mongoose.connect(process.env.MONGO_URI)
         console.log("Banco mongodb conectado!")
     } catch(erro) {
         console.log("erro ao si conectar no banco!")
--- backend/servidor.js
+++ backend/servidor.js
@@ -13,8 +13,8 @@ app.use('/tarefas', router)
 connectDB()
 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log("Servidor rodando")
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
